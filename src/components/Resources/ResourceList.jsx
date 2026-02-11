580b86af60d3be7868d9f865e4cf1ce65fe038a0
gabogara <gabo1gara@gmail.com>
2026-02-10 21:22:42 -0800

    feat: render ResourceList route

--- src/components/Resources/ResourceList.jsx
+++ src/components/Resources/ResourceList.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { Link } from "react-router";
 
-const ResourceList = () => {
-  return <div>ResourceList</div>;
+const ResourceList = ({ resources }) => {
+  return (
+    <main>
+      {resources.map((resource) => (
+        <Link key={resource.id} to={`/resources/${resource.id}`}>
+          <article>
+            <header>
+              <h2>{resource.title}</h2>
+              <p>
+                {resource.createdAt &&
+                  `${new Date(resource.createdAt).toLocaleDateString()}`}
+              </p>
+            </header>
+          </article>
+        </Link>
+      ))}
+    </main>
+  );
 };
 
 export default ResourceList;
