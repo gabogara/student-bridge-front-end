be132de235abce477109540ea30e755a7c6691d7
gabogara <gabo1gara@gmail.com>
2026-02-10 22:50:13 -0800

    feat: add ResourceDetails (fetch show)

--- src/components/Resources/ResourceDetails.jsx
+++ src/components/Resources/ResourceDetails.jsx
@@ -1,9 +1,50 @@
 import { useParams, Link } from "react-router";
+import { useState, useEffect, useContext } from "react";
+
+import * as resourceService from "../../services/resourceService";
+import { UserContext } from "../../contexts/UserContext";
 
 const ResourceDetails = () => {
   const { resourceId } = useParams();
   console.log("resourceId", resourceId);
-  return <div>ResourceDetails</div>;
+  const [resource, setResource] = useState(null);
+  const { user } = useContext(UserContext);
+
+  useEffect(() => {
+    const fetchResource = async () => {
+      const resourceData = await resourceService.show(resourceId);
+      setResource(resourceData);
+    };
+    fetchResource();
+  }, [resourceId]);
+  console.log("resource state:", resource);
+  if (!resource) return <main>Loading...</main>;
+
+  return (
+    <main>
+      <section>
+        <header>
+          <p>{resource.category.toUpperCase()}</p>
+          <h1>{resource.title}</h1>
+          <p>
+            {`${resource.author_username} posted on
+              ${new Date(resource.createdAt).toLocaleDateString()}`}
+          </p>
+
+          {resource.resource_author_id === user.id && (
+            <>
+              <Link to={`/resources/${resourceId}/edit`}>Edit</Link>
+            </>
+          )}
+        </header>
+
+        {resource.description && <p>{resource.description}</p>}
+        <p>{resource.address}</p>
+        <p>{resource.city}</p>
+        {resource.requirements && <p>{resource.requirements}</p>}
+      </section>
+    </main>
+  );
 };
 
 export default ResourceDetails;
