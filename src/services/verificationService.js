5b0d9a915fbf06ef3db826a46881408593de37ff
gabogara <gabo1gara@gmail.com>
2026-02-12 15:50:17 -0800

    feat: add verification service

---
+++ src/services/verificationService.js
@@ -0,0 +1,19 @@
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/resources`;
+
+const create = async (resourceId, verificationFormData) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${resourceId}/verifications`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(verificationFormData),
+    });
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { create };
