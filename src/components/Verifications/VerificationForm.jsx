fe6a4500e25ad3fd195f999f2b67d685565cfd8c
gabogara <gabo1gara@gmail.com>
2026-02-12 16:19:25 -0800

    feat: add verification request form

--- src/components/Verifications/VerificationForm.jsx
+++ src/components/Verifications/VerificationForm.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import { useState } from "react";
 
-const VerificationForm = () => {
-  return <div>VerificationForm</div>;
+const VerificationForm = (props) => {
+  const [formData, setFormData] = useState({ note: "" });
+
+  const handleChange = (evt) => {
+    setFormData({ ...formData, [evt.target.name]: evt.target.value });
+  };
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    props.handleAddVerification(formData);
+    setFormData({ note: "" });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="note-input">Verification note</label>
+      <textarea
+        required
+        name="note"
+        id="note-input"
+        value={formData.note}
+        onChange={handleChange}
+      />
+      <button type="submit">SUBMIT VERIFICATION REQUEST</button>
+    </form>
+  );
 };
 
 export default VerificationForm;
