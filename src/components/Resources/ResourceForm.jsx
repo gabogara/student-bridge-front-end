b8c716e89e0ecaf9a510181d837e647b46428414
gabogara <gabo1gara@gmail.com>
2026-02-13 22:39:55 -0800

    style: update styling for ResourceForm

--- src/components/Resources/ResourceForm.jsx
+++ src/components/Resources/ResourceForm.jsx
@@ -62,77 +62,82 @@ const ResourceForm = (props) => {
 
   return (
     <main className="page">
-      <section className="card">
-        <h1>{resourceId ? "Edit Resource" : "New Resource"}</h1>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="title-input">Title</label>
-          <input
-            required
-            type="text"
-            name="title"
-            id="title-input"
-            value={formData.title}
-            onChange={handleChange}
-          />
+      <div className="form-container">
+        <div className="card">
+          <h1>{resourceId ? "Edit Resource" : "New Resource"}</h1>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="title-input">Title</label>
+            <input
+              required
+              type="text"
+              name="title"
+              id="title-input"
+              value={formData.title}
+              onChange={handleChange}
+            />
 
-          <label htmlFor="description-input">Description</label>
-          <textarea
-            type="text"
-            name="description"
-            id="description-input"
-            value={formData.description}
-            onChange={handleChange}
-          />
+            <label htmlFor="description-input">Description</label>
+            <textarea
+              type="text"
+              name="description"
+              id="description-input"
+              value={formData.description}
+              onChange={handleChange}
+            />
 
-          <label htmlFor="category-input">Category</label>
-          <select
-            required
-            name="category"
-            id="category-input"
-            value={formData.category}
-            onChange={handleChange}
-          >
-            <option value="Food">Food</option>
-            <option value="Housing">Housing</option>
-            <option value="Health">Health</option>
-            <option value="Education">Education</option>
-          </select>
+            <label htmlFor="category-input">Category</label>
+            <select
+              required
+              name="category"
+              id="category-input"
+              value={formData.category}
+              onChange={handleChange}
+            >
+              <option value="Food">Food</option>
+              <option value="Housing">Housing</option>
+              <option value="Health">Health</option>
+              <option value="Education">Education</option>
+            </select>
 
-          <label htmlFor="address-input">Address</label>
-          <input
-            required
-            type="text"
-            name="address"
-            id="address-input"
-            value={formData.address}
-            onChange={handleChange}
-          />
+            <label htmlFor="address-input">Address</label>
+            <input
+              required
+              type="text"
+              name="address"
+              id="address-input"
+              value={formData.address}
+              onChange={handleChange}
+            />
 
-          <label htmlFor="city-input">City</label>
-          <input
-            required
-            type="text"
-            name="city"
-            id="city-input"
-            value={formData.city}
-            onChange={handleChange}
-          />
+            <label htmlFor="city-input">City</label>
+            <input
+              required
+              type="text"
+              name="city"
+              id="city-input"
+              value={formData.city}
+              onChange={handleChange}
+            />
 
-          <label htmlFor="requirements-input">Requirements</label>
-          <textarea
-            type="text"
-            name="requirements"
-            id="requirements-input"
-            value={formData.requirements}
-            onChange={handleChange}
-          />
-
-          <button type="submit">SUBMIT</button>
-          <button type="button" onClick={() => navigate(from)}>
-            Cancel
-          </button>
-        </form>
-      </section>
+            <label htmlFor="requirements-input">Requirements</label>
+            <textarea
+              type="text"
+              name="requirements"
+              id="requirements-input"
+              value={formData.requirements}
+              onChange={handleChange}
+            />
+            <div className="button-row">
+              <button className="primary" type="submit">
+                SUBMIT
+              </button>
+              <button type="button" onClick={() => navigate(from)}>
+                Cancel
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </main>
   );
 };
