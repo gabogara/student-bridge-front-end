1ff22b95110eb093232b63e566ce31f7c8c0a065
gabogara <gabo1gara@gmail.com>
2026-02-16 19:40:03 -0800

    feat: add /saves endpoint  and scaffolding for saved resources

---
+++ src/components/Saves/SavedResources.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SavedResources = () => {
+  return (
+    <div>
+      <p>SavedResources is comming...</p>
+    </div>
+  );
+};
+
+export default SavedResources;
